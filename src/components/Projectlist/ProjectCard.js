fdd986951ced6e572bc9dbc894dabc15cd7a86a8
Rahul <coderrahul96@gmail.com>
2023-07-12 16:35:09 +0530

    card updated

--- src/components/Projectlist/ProjectCard.js
+++ src/components/Projectlist/ProjectCard.js
@@ -8,7 +8,7 @@ function ProjectCard(props) {
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
+      <Card.Body className="d-flex flex-column justify-content-between">
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}

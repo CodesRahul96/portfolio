cff7697ddcdaaed25b2cf4694c0dfdacbd7415ad
Rahul <105583354+coderrahul-github@users.noreply.github.com>
2023-12-06 18:00:01 +0530

    layout updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React  from 'react';
 import { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Preloader from '../src/components/Pre'
